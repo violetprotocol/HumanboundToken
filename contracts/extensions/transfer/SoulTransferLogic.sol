cf15053dab25719a024b2f7bc3a5d8623d9e0360
Papa Smurf <chris@violet.co>
2022-09-02 13:31:15 +0100

    feat: refactored SoulTransferLogic

--- contracts/extensions/transfer/SoulTransferLogic.sol
+++ contracts/extensions/transfer/SoulTransferLogic.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
-import "@violetprotocol/erc721extendable/contracts/extensions/base/transfer/TransferLogic.sol";
 import "../EAT/AccessTokenConsumerExtension.sol";
 import "./ISoulTransferLogic.sol";
 
-contract SoulTransferLogic is ISoulTransferLogic, TransferLogic, AccessTokenConsumerExtension {
+contract SoulTransferLogic is SoulTransferExtension, AccessTokenConsumerExtension {
     /**
      * @dev See {ITransferLogic-transferFrom}.
      */
@@ -75,21 +74,5 @@ contract SoulTransferLogic is ISoulTransferLogic, TransferLogic, AccessTokenCons
         bytes memory data
     ) public override(ISoulTransferLogic) requiresAuth(v, r, s, expiry) {
         _safeTransfer(from, to, tokenId, data);
-    }
-
-    function getInterfaceId() public pure virtual override(TransferLogic, Extension) returns (bytes4) {
-        return (type(ISoulTransferLogic).interfaceId);
-    }
-
-    function getInterface() public pure virtual override(TransferLogic, IExtension) returns (string memory) {
-        return
-            string(
-                abi.encodePacked(
-                    TransferLogic.getInterface(),
-                    "function transferFrom(uint8 v, bytes32 r, bytes32 s, uint256 expiry, address from, address to, uint256 tokenId) external;\n"
-                    "function safeTransferFrom(uint8 v, bytes32 r, bytes32 s, uint256 expiry, address from, address to, uint256 tokenId) external;\n"
-                    "function safeTransferFrom(uint8 v, bytes32 r, bytes32 s, uint256 expiry, address from, address to, uint256 tokenId, bytes memory data) external;\n"
-                )
-            );
     }
 }
