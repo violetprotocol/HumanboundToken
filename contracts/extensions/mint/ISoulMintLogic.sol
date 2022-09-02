c496c835952c8ed9809b2aaf08bf9453699e8cef
Papa Smurf <chris@violet.co>
2022-09-02 12:22:28 +0100

    feat: refactored SoulMintLogic

--- contracts/extensions/mint/ISoulMintLogic.sol
+++ contracts/extensions/mint/ISoulMintLogic.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
+import "@violetprotocol/erc721extendable/contracts/extensions/base/mint/Mint.sol";
+
 interface ISoulMintLogic {
     function mint(
         uint8 v,
@@ -11,4 +13,25 @@ interface ISoulMintLogic {
         uint256 tokenId,
         string calldata tokenURI
     ) external;
+}
+
+abstract contract SoulMintExtension is ISoulMintLogic, Extension {
+    /**
+     * @dev see {IExtension-getSolidityInterface}
+     */
+    function getSolidityInterface() public pure virtual override returns (string memory) {
+        return "function mint(uint8 v, bytes32 r, bytes32 s, uint256 expiry, address to, uint256 tokenId) external;\n";
+    }
+
+    /**
+     * @dev see {IExtension-getInterface}
+     */
+    function getInterface() public virtual override returns (Interface[] memory interfaces) {
+        interfaces = new Interface[](1);
+
+        bytes4[] memory functions = new bytes4[](1);
+        functions[0] = ISoulMintLogic.mint.selector;
+
+        interfaces[0] = Interface(type(ISoulMintLogic).interfaceId, functions);
+    }
 }
