857274f57aaf2feacfbb576a6c547f960a0ed1df
Papa Smurf <chris@violet.co>
2022-10-05 16:06:43 +0100

    fix: remove unused import

--- contracts/extensions/approve/HumanboundApproveLogic.sol
+++ contracts/extensions/approve/HumanboundApproveLogic.sol
@@ -2,7 +2,6 @@
 pragma solidity >=0.8.13;
 
 import "@violetprotocol/erc721extendable/contracts/extensions/base/approve/ApproveLogic.sol";
-import { HumanboundPermissionState, HumanboundPermissionStorage } from "../../storage/HumanboundPermissionStorage.sol";
 
 contract HumanboundApproveLogic is ApproveLogic {
     function approve(address to, uint256 tokenId) public pure override {
