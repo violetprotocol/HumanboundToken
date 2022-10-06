fcd4e8f3995e6924adb334b2170474049b0334ef
Papa Smurf <chris@violet.co>
2022-10-06 11:51:12 +0100

    feat: modified humanboundExtend to be callable by self, added retract and replace

--- contracts/extensions/replace/HumanboundReplaceLogic.sol
+++ contracts/extensions/replace/HumanboundReplaceLogic.sol
@@ -1,36 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
-import "@violetprotocol/extendable/extensions/extend/ExtendLogic.sol";
+import "@violetprotocol/extendable/extensions/replace/StrictReplaceLogic.sol";
 import { HumanboundPermissionState, HumanboundPermissionStorage } from "../../storage/HumanboundPermissionStorage.sol";
 
-contract HumanboundExtendLogic is ExtendLogic {
+contract HumanboundReplaceLogic is StrictReplaceLogic {
     event OperatorInitialised(address initialOperator);
 
     modifier onlyOperatorOrSelf() virtual {
-        initialise();
-
         HumanboundPermissionState storage state = HumanboundPermissionStorage._getState();
-
-        // Set the operator to the transaction sender if operator has not been initialised
-        if (state.operator == address(0x0)) {
-            state.operator = _lastCaller();
-            emit OperatorInitialised(_lastCaller());
-        }
 
         require(
             _lastCaller() == state.operator || _lastCaller() == address(this),
-            "HumanboundExtendLogic: unauthorised"
+            "HumanboundReplaceLogic: unauthorised"
         );
         _;
     }
 
     // Overrides the previous implementation of modifier to remove owner checks
-    modifier onlyOwnerOrSelf() override {
+    modifier onlyOwner() override {
         _;
     }
 
-    function extend(address extension) public override onlyOperatorOrSelf {
-        super.extend(extension);
+    function replace(address oldExtension, address newExtension) public override onlyOperatorOrSelf {
+        super.replace(oldExtension, newExtension);
     }
 }
