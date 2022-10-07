6fe5eadb584b11d7c9e43cbb5020e42eaecefd20
Papa Smurf <chris@violet.co>
2022-10-07 11:39:45 +0100

    fix: reverted task param name

--- tasks/deploy/extension.ts
+++ tasks/deploy/extension.ts
@@ -5,17 +5,17 @@ import { TaskArguments } from "hardhat/types";
 import { deploy, deployWithLedger } from "../helpers";
 
 task("deploy:extension", "Deploys a specified constructorless Extension")
-  .addParam("extensionName", "Extension contract name to deploy. Must be constructorless.")
+  .addParam("name", "Extension contract name to deploy. Must be constructorless.")
   .setAction(async function (taskArguments: TaskArguments, { ethers }) {
-    const contract = await deploy(ethers, taskArguments.extensionName);
-    console.log(`${taskArguments.extensionName} deployed to: `, contract.address);
+    const contract = await deploy(ethers, taskArguments.name);
+    console.log(`${taskArguments.name} deployed to: `, contract.address);
   });
 
 task("hd:deploy:extension", "Deploys a specified constructorless Extension")
-  .addParam("extensionName", "Extension contract name to deploy. Must be constructorless.")
+  .addParam("name", "Extension contract name to deploy. Must be constructorless.")
   .setAction(async function (taskArguments: TaskArguments, { ethers, network }) {
     const ledger = new LedgerSigner(ethers.provider);
 
-    const contract = await deployWithLedger(ledger, network, ethers, taskArguments.extensionName);
-    console.log(`${taskArguments.extensionName} deployed to: `, contract.address);
+    const contract = await deployWithLedger(ledger, network, ethers, taskArguments.name);
+    console.log(`${taskArguments.name} deployed to: `, contract.address);
   });
